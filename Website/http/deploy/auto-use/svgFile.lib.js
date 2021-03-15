65668ebb6911fec471fccdc4946537ad3234a166
Thomas Lochmatter <thomas.lochmatter@viereck.ch>
2021-03-15 13:22:13 +0100

    Added new deploy script: - use "deploy/run" for testing on preview.condensation.io - and "deploy/run publish" to publish on the main website Added include/svgFile example in /matteo Removed all .DS_Store (and added a corresponding .gitignore) Removed all generated files (index.html, style.css) from GIT, leaving only the source files (index.build.html)

--- Website/http/deploy/auto-use/svgFile.lib.js
+++ Website/http/deploy/auto-use/svgFile.lib.js
@@ -18,7 +18,7 @@ function svgFile(file, options) {
 	delete svg.attributes['xmlns:svg'];
 	delete svg.attributes['xmlns:sodipodi'];
 
-	//svg.removeChildrenWithTag('defs');
+	svg.removeChildrenWithTag('defs');
 	svg.removeChildrenWithTag('metadata');
 	svg.removeChildrenWithTag('sodipodi:namedview');
 	svg.removeChildrenWith(node => node.attributes && node.attributes.id && node.attributes.id.match(/[A-Za-z0-9]+(Ruler|Color)/));
@@ -26,21 +26,20 @@ function svgFile(file, options) {
 
 	// Apply options
 	if (options) {
-		applyAttribute('id');
 		applyAttribute('style');
 		applyAttribute('width');
 		applyAttribute('height');
 		applyAttribute('viewBox');
-		applyAttribute('className', 'class');
+		applyAttribute('class');
+		applyAttribute('alt');
 		if (options.process) options.process(svg);
 	}
 
-	function applyAttribute(name, attributeName) {
-		if (! attributeName) attributeName = name;
+	function applyAttribute(name) {
 		var value = options[name];
 		if (value === undefined) return;
-		if (value == null) delete svg.attributes[attributeName];
-		else svg.attributes[attributeName] = value;
+		if (value == null) delete svg.attributes[name];
+		else svg.attributes[name] = value;
 	}
 
 	// Insert it
@@ -52,7 +51,7 @@ function svgFile(file, options) {
 		//delete node.attributes['xml:space'];
 
 		// Remove default IDs
-		if (node.attributes.id != null && node.attributes.id.match(/^(layer|text|tspan|rect|path|g|circle|defs|svg|a|polygon|image)\d{1,4}$/))
+		if (node.attributes.id != null && node.attributes.id.match(/^(text|tspan|rect|path|g|circle|defs|svg|a|polygon|image)\d{1,4}$/))
 			delete node.attributes['id'];
 
 		// Remove inkscape-specific attributes
@@ -63,11 +62,9 @@ function svgFile(file, options) {
 
 		// Fix hrefs
 		if (node.attributes['xlink:href']) {
+			var folder = path.dirname(fullPath);
 			var link = node.attributes['xlink:href'];
-			if (link[0] != '#' && link.indexOf(':') < 0) {
-				var folder = path.dirname(file);
-				node.attributes['xlink:href'] = path.join(folder, link);
-			}
+			node.attributes['xlink:href'] = path.join(folder, link);
 		}
 
 		// Clean the style
@@ -98,63 +95,6 @@ function svgFile(file, options) {
 
 	function cleanStyle(style) {
 		delete style['-inkscape-font-specification'];
-		delete style['clip-rule'];
-		delete style['color'];
-		delete style['color-interpolation'];
-		delete style['color-interpolation-filters'];
-		delete style['color-rendering'];
-		delete style['enable-background'];
-		delete style['fill-rule'];
-		delete style['image-rendering'];
-		delete style['isolation'];
-		delete style['marker'];
-		delete style['mix-blend-mode'];
-		delete style['overflow'];
-		delete style['shape-rendering'];
-		delete style['solid-color'];
-		delete style['solid-opacity'];
-		delete style['text-rendering'];
-		delete style['vector-effect'];
-		delete style['visibility'];
-
-		if (style['stroke'] == 'none' || style['stroke-dasharray'] == 'none') {
-			delete style['stroke-dasharray'];
-			delete style['stroke-dashoffset'];
-		}
-
-		if (style['stroke'] == 'none') {
-			delete style['stroke'];
-			delete style['stroke-linecap'];
-			delete style['stroke-linejoin'];
-			delete style['stroke-miterlimit'];
-			delete style['stroke-width'];
-		}
-
-		removeDefault('stroke-linecap', 'butt');
-		removeDefault('stroke-linejoin', 'miter');
-		removeDefault('stroke-miterlimit', '4');
-		removeDefault('stroke-opacity', '1');
-		removeDefault('fill-opacity', '1');
-		removeDefault('opacity', '1');
-		removeDefault('display', 'inline');
-
-		cleanFont();
-
-		function removeDefault(name, value) {
-			if (style[name] == value) delete style[name];
-		}
-
-		function cleanFont() {
-			var fontFamily = style['font-family'];
-			if (! fontFamily) return;
-			if (fontFamily == 'Lato') {
-				delete style['font-family'];
-				return;
-			}
-
-			if (fontFamily == '\'Source Code Pro\'') return;
-			if (fontFamily == '\'Noto Sans Meroitic\'') return;
-			logOrange(file + ' uses font ' + fontFamily);
-		}
+		if (style['font-family'] != 'monospace') delete style['font-family'];
 	}
 }
