304a852fdea2178b89ccdeebaf6d37ecff52fc6a
LLocci <83076565+LLocci@users.noreply.github.com>
2022-01-07 08:40:05 +0100

    Delete Website directory

--- Website/http/deploy/auto-use/gitRevision.lib.js
+++
@@ -1,5 +0,0 @@
-function gitRevision(length) {
-	var options = {stdio: 'pipe', encoding: 'utf-8'};
-	var result = child_process.spawnSync('git', ['rev-parse', 'HEAD'], options);
-	return '\'' + result.stdout.trim().substr(0, length) + '\'';
-}
